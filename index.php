a4359acb376c9f2d9dd9a1b0060aeb8d95e1d4cf
citrino88 <almeida.citrino@gmail.com>
2024-06-14 17:07:50 -0300

    Criação inicial das pastas e Mobile First

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mobile First</title>
+    <link rel="stylesheet" href="/css/style.css" media="all">
+    <link rel="stylesheet" href="/css/media-query.css">
+</head>
+<body>
+    <h1>Mobile First</h1>
+</body>
+</html>
