29a3fe559029f89d2b485925cc620720ae145617
citrino88 <almeida.citrino@gmail.com>
2024-06-18 16:32:54 -0300

    inicio bolos

---
+++ bolos.php
@@ -0,0 +1,11 @@
+<article class="destaque">
+        <h2>BOLOS CASEIROS</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, illum!</p>
+        <figure><img src="css/imagens/im-fundo.jpg" ialt="Imagem de um doce"></figure>
+    </article>
+    <div class="categorias" >
+    <h2>Selecione a categoria</h2>
+<a href="categoria-doces.html" class="caixa">DOCES</a>
+<a href="categoria-paesArtesanais.html" class="caixa">PÃES ARTESANAIS</a>
+<a href="categoria-salgados.html" class="caixa">SALGADOS</a>
+<a href="categoria-doces.html" class="caixa">BOLOS CASEIROS</a>
