fcb456ba6ffc3570ced445b78074fb4c3eae9b7c
citrino88 <almeida.citrino@gmail.com>
2024-06-18 14:49:48 -0300

    cabecalho e rodape 01

---
+++ cabecalho.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br" class="h-100">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Panificação e Bolos</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body class="d-flex flex-column h-100">
+    
+<header id="topo" class="border-bottom sticky-top">
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Cake</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Página Inicial</a>
+        </li>
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Categorias
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Doces</a></li>
+            <li><a class="dropdown-item" href="#">Salgados</a></li>
+            <li><a class="dropdown-item" href="#">Pães</a></li>
+            <li><a class="dropdown-item" href="#">Bolos</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <!-- <li><a class="dropdown-item" href="#">Something else here</a></li> -->
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contato</a>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li> -->
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+</header>
+
+<main class="flex-shrink-0">
+    <div class="container">
--- index.php
+++ index.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mobile First</title>
-    <link rel="stylesheet" href="/css/style.css" media="all">
-    <link rel="stylesheet" href="/css/media-query.css">
-</head>
-<body>
-    <main>
-    <h1>Mobile First</h1>
-    </main>
-    </body>
-</html>
+<?php 
+require "cabecalho.php"; 
+?>  
+
+
+
+<?php 
+require_once "rodape.php";
+?>
