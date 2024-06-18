edcb4a5670f61d2f3ba759d23aabe617ae2c671e
citrino88 <almeida.citrino@gmail.com>
2024-06-18 17:02:13 -0300

    outras paginas de categorias

--- bolos.php
+++ bolos.php
@@ -1,11 +1,29 @@
+<?php
+require "cabecalho.php";
+?>
+
 <article class="destaque">
-        <h2>BOLOS CASEIROS</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, illum!</p>
-        <figure><img src="css/imagens/im-fundo.jpg" ialt="Imagem de um doce"></figure>
-    </article>
-    <div class="categorias" >
+    <h2>BOLOS CASEIROS</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, illum!</p>
+    <figure><img src="img/im-fundo.jpg" class="img-fluid" ialt="Imagem de um doce"></figure>
+</article>
+
+<div class="categorias">
     <h2>Selecione a categoria</h2>
-<a href="categoria-doces.html" class="caixa">DOCES</a>
-<a href="categoria-paesArtesanais.html" class="caixa">PÃES ARTESANAIS</a>
-<a href="categoria-salgados.html" class="caixa">SALGADOS</a>
-<a href="categoria-doces.html" class="caixa">BOLOS CASEIROS</a>
+    <a href="paes.php" class="caixa">PÃES ARTESANAIS</a>
+    <a href="doces.php" class="caixa">DOCES</a>    
+    <a href="salgados.php" class="caixa">SALGADOS</a>
+    <a href="bolos.php" class="caixa">BOLOS CASEIROS</a>
+</div>    
+
+<div id="btn-categorias" class="d-grid gap-2">
+  <button class="btn btn-primary" type="button">Pães Artesanais</button>
+  <button class="btn btn-primary" type="button">Doces</button>
+  <button class="btn btn-primary" type="button">Salgados</button>
+  <button class="btn btn-primary" type="button">Bolos Caseiros</button>
+</div>
+
+
+    <?php
+    require_once "rodape.php";
+    ?>
