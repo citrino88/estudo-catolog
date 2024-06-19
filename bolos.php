150fe8c9984f802562b9f9a0470a5d17e5aa7145
citrino88 <almeida.citrino@gmail.com>
2024-06-19 15:53:16 -0300

    novas imagens

--- bolos.php
+++ bolos.php
@@ -31,16 +31,16 @@ Aqui é pra colocar uma imagem banner
 
 <hr>
 
-<div class="row">
-<div class="col card" style="width: 18rem;">
-  <img src="img/bolo.jpg" class="card-img-top" alt="...">
+<div class="row p-2">
+<div class="col card mx-2" style="width: 18rem;">
+  <img src="img/bolo-chocolate-maracuja.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
-<div class="col card" style="width: 18rem;">
+<div class="col card mx-2" style="width: 18rem;">
   <img src="img/bolo.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Bolo de Creme com Frutas</h5>
